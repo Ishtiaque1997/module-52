305a5230a28f8632c5eb2f76440e947702db3063
Ishtiaque1997 <60317006+Ishtiaque1997@users.noreply.github.com>
2021-10-23 21:27:37 +0600

    some done

--- src/App.js
+++ src/App.js
@@ -6,16 +6,29 @@ import {
   Link
 } from "react-router-dom";
 import Home from './Components/Home/Home';
+import Friends from './Components/Friends/Friends';
+import About from './Components/About/About';
+import NotFound from './Components/NotFound/NotFound'
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route exact path='/home'>
+          <Route exact path='/'>
                  <Home></Home>
           </Route>
-          <Route></Route>
-          <Route></Route>
+           <Route path='/home'>
+                 <Home></Home>
+          </Route>
+          <Route path='/friends'>
+             <Friends></Friends>
+          </Route>
+          <Route path='/about'>
+               <About></About>
+          </Route>
+           <Route path='*'>
+               <NotFound></NotFound>
+          </Route>
         </Switch>
       </Router>
      
