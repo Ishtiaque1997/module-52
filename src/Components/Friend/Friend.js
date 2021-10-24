a1a26ef004fd0653550ce9c4fe4dc9e7676bd8f0
Ishtiaque1997 <60317006+Ishtiaque1997@users.noreply.github.com>
2021-10-24 07:16:14 +0600

    almost done

--- src/Components/Friend/Friend.js
+++ src/Components/Friend/Friend.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link,useHistory } from 'react-router-dom';
 
 const Friend = (props) => {
  const{name,id,phone,address}=props.friend;
+ const history=useHistory();
  const friendStyle={
   border:'3px solid goldenrod',
   padding:'10px',
   borderRadius:'10px'
 
  }
+ const handleFriendClick=()=>{
+   history.push(`/friend/${id}`)
+ }
+
  const url=`/friend/${id}`;
  return (
   <div style={friendStyle}>
@@ -16,6 +21,12 @@ const Friend = (props) => {
    <h5>Call me :{phone}</h5>
    <p><small>{address.city}</small></p>
    <Link to ={url}>Visit me</Link>
+   <br />
+   {/* <Link to={url}>
+    <button>Visit me</button>
+   </Link> */}
+   <br />
+   <button onClick={handleFriendClick}>Visit me 2</button>
   </div>
  );
 };
--- src/Components/FriendDetail/FriendDetail.js
+++ src/Components/FriendDetail/FriendDetail.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useEffect } from 'react';
-import { useParams } from 'react-router';
+import { useHistory, useParams } from 'react-router';
 
 const FriendDetail = () => {
  const {friendId}=useParams();
@@ -11,6 +11,10 @@ const FriendDetail = () => {
   .then(res=>res.json())
   .then(data=>setFriend(data))
  },[])
+ const history=useHistory();
+ const handleSeeFriend=()=>{
+   history.push('/friends')
+ }
  return (
   <div>
    <h5>Detail of {friendId} </h5>
@@ -18,6 +22,7 @@ const FriendDetail = () => {
    <h4>{friend.email}</h4>
    <h6>{friend.phone}</h6>
    <p>{friend.company?.name}</p>
+   <button onClick={handleSeeFriend}>See all friends</button>
   </div>
  );
 };
